8031c3de77798a1c2e3bdfa52f3a610166fa5e6d
ajith11031999 <ajith11031999@gmail.com>
2020-01-08 16:40:15 +0530

    perl try

---
+++ try/aj7.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+@names=('xavi','messi','ronaldo','zaltan','ajith');
+print"@names\n";
+@names=sort(@names);
+print"@names\n";
+@array1=(1..5);
+@array2=(6..10);
+@merged_array =(@array1,@array2);
+print"@merged_array\n";
+
---
+++ try/aj9.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+%tennis=(1=>"novak",2=>"roger",3=>"andy");
+@ranks=keys%tennis;
+@values=values%tennis;
+print"keys:@ranks\n";
+print"values:@values\n";
